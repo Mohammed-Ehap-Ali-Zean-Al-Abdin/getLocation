fbee081718a109550d9687a922307b4ccc478d15
Mohammed-Ehap-Ali-Zean-Al-Abdin <113460530+Mohammed-Ehap-Ali-Zean-Al-Abdin@users.noreply.github.com>
2023-02-27 21:51:08 +0200

    commit

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 window.onload = ()=>{
-    fetch("https://ipapi.co/json/")
-    .then((response)=>response.json())
-    .then((data)=>{
-        console.log(data);
-        document.write(`${data.latitude} ${data.longitude}`);
+    // fetch("https://ipapi.co/json/")
+    // .then((response)=>response.json())
+    // .then((data)=>{
+    //     console.log(data);
+    //     document.write(`${data.latitude} ${data.longitude}`);
+    // });
+    navigator.geolocation.getCurrentPosition((position)=>{
+        console.log(position);
+        document.write(`${position.coords.longitude} ${position.coords.latitude}`);
     });
 };
